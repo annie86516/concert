3019ba585b9abfda5d71d9dcc2c1f02e64deb687
annie86516 <annie86516@gmail.com>
2026-09-04 00:04:30 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "group-ticket-v8";
+const CACHE_NAME = "group-ticket-v9";
 const APP_FILES = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", event => {
